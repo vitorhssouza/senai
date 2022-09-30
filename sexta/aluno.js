d26684bb6ab50916fe3584c97858882c8086e26f
vitor souza <vitorugo_kta@hotmail.com>
2022-09-30 19:34:12 -0300

    Adicionando arquivo aluno

---
+++ sexta/aluno.js
@@ -0,0 +1,18 @@
+// CLasse 
+// OBS o modulo exports serve para outras classes poderem utilizalas 
+
+module.exports = class Aluno{
+    // Caracteristica = Propriedades do aluno
+    constructor(matricula, nome, idade, nota){
+        this.matricula = matricula
+        this.nome = nome
+        this.idade = idade
+        this.nota = nota
+    }
+
+    maior(){
+        return this.idade >= 18;
+    }
+    
+
+}
