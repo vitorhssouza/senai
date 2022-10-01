d387e17079cd237bd9a5db840e49eb6a13b1499d
vitor souza <vitorugo_kta@hotmail.com>
2022-09-30 21:17:06 -0300

    adicionando classe cliente

---
+++ sexta/cliente.js
@@ -0,0 +1,9 @@
+module.exports = class Cliente{
+    constructor(nome, idade, email, endereco){
+        this.nome = nome
+        this.idade = idade
+        this.email = email
+        this.endereco = endereco
+    }
+
+}
